91bba8b0ae27e10f2b33a9e7974500f6eb5bd822
massawegodii <massawegodfrey43@gmail.com>
2023-02-28 00:33:40 +0300

    first commit

---
+++ main/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Student)
+admin.site.register(AdminUser)
+admin.site.register(Result)
+admin.site.register(Subject)
+admin.site.register(Dept)
+admin.site.register(RegisterTable)
+admin.site.register(Teacher)
+admin.site.register(AssignedTeacher)
+admin.site.register(AssignedTeacher2)
+admin.site.register(Rating)
+admin.site.register(Course)
+admin.site.register(Module)
+
+
+# Register your models here.
+
